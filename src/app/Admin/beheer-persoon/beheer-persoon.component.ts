9d91c752f5fb7eb8c9f5da9a493889ca1d6ec7c2
Maarten Michiels <mnmmaarten@hotmail.com>
2020-01-23 12:56:06 +0100

    Added Admin beheer-persoon

--- src/app/Admin/beheer-persoon/beheer-persoon.component.ts
+++ src/app/Admin/beheer-persoon/beheer-persoon.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Type} from '../../Models/type.model';
+import {Router} from '@angular/router';
+import {Persoon} from '../../Models/persoon.model';
+import {PersoonService} from '../../Services/persoon.service';
+import {TypeService} from '../../Services/type.service';
 
 @Component({
   selector: 'app-beheer-persoon',
@@ -7,9 +12,99 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeheerPersoonComponent implements OnInit {
 
-  constructor() { }
+  wachtwoordHerhaling: string = '';
+
+  personen: Persoon[];
+  types: Type[];
+  persoon: Persoon = new Persoon();
+  popupNewPersoon: boolean = false;
+  // popupEditPersoon: boolean = false;
+
+
+  constructor(
+    private router: Router,
+    private persoonService: PersoonService,
+    private typeService: TypeService
+  ) {
+    this.ReadPersonen();
+    this.ReadTypes();
+  }
+
+  private ReadTypes() {
+    this.typeService.getTypes().subscribe(r => {
+      this.types = r;
+      console.log(this.types);
+    });
+  }
+
+  private ReadPersonen() {
+    this.persoonService.getPersonen().subscribe(r => {
+      this.personen = r;
+      console.log(this.personen);
+    });
+  }
+
+  closePopups() {
+    this.popupNewPersoon = false;
+    // this.popupEditPersoon = false;
+  }
 
   ngOnInit() {
+  }
+
+  OpenNewPersoonForm() {
+    this.persoon.persoonID = 0;
+    this.persoon.voornaam = null;
+    this.persoon.naam = null;
+    this.persoon.usernaam = null;
+    this.persoon.wachtwoord = null;
+    this.persoon.typeID = 0;
+    this.popupNewPersoon = true;
+    // this.popupEditPersoon = false;
+  }
+
+
+  OpenEditPersoonForm(persoonID: number) {
+
+    this.persoonService.getPersoonByID(persoonID).subscribe(r => {
+      this.persoon = r;
+      console.log('type: ', this.persoon);
+      // this.popupEditPersoon = true;
+      this.popupNewPersoon = true;
+    });
+  }
+
+
+  onSubmit() {
+    if (this.persoon.persoonID === 0) {
+      console.log('post', this.persoon);
+      this.persoonService.addPersoon(this.persoon).subscribe(r => {
+        this.persoon.persoonID = r.persoonID;
+        console.log('type: ', this.persoon);
+        this.ReadPersonen();
+      });
+    } else {
+      console.log('put', this.persoon);
+      this.persoonService.updatePersoon(this.persoon).subscribe(r => {
+        console.log('succes');
+        this.ReadPersonen();
+      });
+    }
+
+    this.closePopups();
+  }
+
+  deleteTypeAlert(persoon: Persoon) {
+    if (confirm('Ben jezeker dat je ' + persoon.naam + ', ' + persoon.voornaam + ' wilt verwijderen?')) {
+      this.deletePersoon(persoon.persoonID);
+    }
+  }
+
+  deletePersoon(persoonID: number) {
+    this.persoonService.deletePersoon(persoonID).subscribe(result => {
+      // this.popupEditPersoon = false;
+      this.ReadPersonen();
+    });
   }
 
 }
--- src/app/Models/persoon.model.ts
+++ src/app/Models/persoon.model.ts
@@ -1,13 +1,12 @@
-import {Plaats} from "./plaats.model";
-import {Melding} from "./melding.model";
-import {Type} from "./type.model";
+import {Melding} from './melding.model';
+import {Type} from './type.model';
 
 export class Persoon {
   persoonID: number;
   voornaam: String;
   naam: String;
   usernaam: String;
-  TypeID: number;
+  typeID: number;
   wachtwoord: String;
   meldingen: Melding[];
   type: Type;
