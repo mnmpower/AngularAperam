0628b166d583ec016b1c72f52d1e400fb0f903d2
Maarten Michiels <mnmmaarten@hotmail.com>
2020-01-27 16:20:14 +0100

    added live chart

--- src/app/Admin/live-chart/live-chart.component.ts
+++ src/app/Admin/live-chart/live-chart.component.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-live-chart',
   templateUrl: './live-chart.component.html',
-  styleUrls: ['./live-chart.component.scss']
+  styleUrls: ['./live-chart.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+      transition('* => closed', [
+        animate('1s')
+      ]),
+      transition('* => open', [
+        animate('0.5s')
+      ]),
+      transition('open <=> closed', [
+        animate('0.5s')
+      ]),
+      transition('* => open', [
+        animate('1s',
+          style({opacity: '*'}),
+        ),
+      ]),
+      transition('* => *', [
+        animate('1s')
+      ]),
+    ]),
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'translateX(100%)' }))
+      ])
+    ]),
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('5s', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('5s', style({ opacity: 0 }))
+      ])
+    ]),
+  ]
 })
+
 export class LiveChartComponent implements OnInit {
 
-  constructor() { }
+  isOpen = true;
+  show = true;
+
+  constructor() {
+
+    this.show = false;
+  }
 
   ngOnInit() {
   }
+
+  toggle() {
+    this.show = !this.show;
+    console.log(this.show);
+  }
+
 
 }
