6a35b4011c850489a99bd6d36cdbc421ee70becf
Maarten Michiels <mnmmaarten@hotmail.com>
2020-02-12 13:37:13 +0100

    Added auto refresh on Melding tab

--- src/app/User/meldingen/meldingen.component.ts
+++ src/app/User/meldingen/meldingen.component.ts
@@ -1,26 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import {MeldingService} from "../../Services/melding.service";
-import {Melding} from "../../Models/melding.model";
-import {Observable} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MeldingService} from '../../Services/melding.service';
+import {Melding} from '../../Models/melding.model';
+import {interval, Observable, Subscription} from 'rxjs';
+
+const source = interval(5000);
 
 @Component({
   selector: 'app-meldingen',
   templateUrl: './meldingen.component.html',
   styleUrls: ['./meldingen.component.scss']
 })
-export class MeldingenComponent implements OnInit {
+export class MeldingenComponent implements OnInit, OnDestroy {
 
   meldingen: Melding[] = [];
+  nieuweMeldingen: Melding[] = [];
+
+
+  subscription: Subscription;
 
   constructor(
     private meldingService: MeldingService,
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
-   this.meldingService.getMeldingen().subscribe(r => {
+    this.meldingService.getMeldingen().subscribe(r => {
       console.log(r);
       this.meldingen = r;
-   });
+      this.refresh();
+    });
+  }
+
+  refresh() {
+    this.subscription = source.subscribe(val => this.meldingService.getMeldingen().subscribe(r => {
+        console.log(r);
+        this.nieuweMeldingen = r;
+        if (this.nieuweMeldingen.length !== this.meldingen.length) {
+          this.meldingen = this.nieuweMeldingen;
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
